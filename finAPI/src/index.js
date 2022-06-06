152e6910e732cb8c77eef99a0d56072d4f584579
deboraamartinez <deboraaspesimartinez@gmail.com>
2022-06-06 15:19:48 -0300

    feat: controllers get and post

--- finAPI/src/index.js
+++ finAPI/src/index.js
@@ -1,29 +1,74 @@
-const { response } = require('express')
 const express = require('express')
 const port = 3000
+const { v4: uuidv4 } = require('uuid')
+
 const app = express()
+app.use(express.json())
+
+const customers = []
+
+// Middleware
+
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers
+  const customer = customers.find((customer) => customer.cpf === cpf)
+  if (!customer) {
+    return res
+      .status(400)
+      .json({ error: 'Customer not found' })
+  }
+  req.customer = customer
+  return next()
+
+}
 
 
-app.get('/', (req, res) => {
-  return res.json()
+app.post('/account', verifyIfExistsAccountCPF, (req, res) => {
+  const { cpf, name } = req.body
+  const customerAlreadyExists = customers.some((custumer) => custumer.cpf === cpf)
+
+  if (customerAlreadyExists) {
+    return res
+      .status(400)
+      .json({ error: "Costumer already exists" })
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  })
+
+  return res
+    .status(201)
+    .send()
 })
 
-app.post('/', (req, res) => {
-  return res.json()
+app.get('/statement', verifyIfExistsAccountCPF, (req, res) => {
+
+  const { customer } = req
+  return res
+    .status(200)
+    .json(customer.statement)
 })
 
-app.put('//:id', (req, res) => {
-  return res.json()
-})
-app.patch('//:id', (req, res) => {
-  return res.json()
-})
+app.post('/deposit', verifyIfExistsAccountCPF, (req, res) => {
+  const { description, amount } = request.body
 
-app.delete('//:id', (req, res) => {
-  return res.json()
+  const { customer } = req
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  }
+  customer.statement.push(statementOperation)
+  return res
+    .status(201)
+    .send()
 })
-
-
 
 
 app.listen(port, () => {
